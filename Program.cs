462563e75b47c2cdbe30476239b71b52eebdfd8b
valt449 <nicolaj.iversen@gmail.com>
2016-12-10 20:49:49 +0100

    Initial check-in of module ExamPrep

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Nico
+{
+
+    public struct something
+    {
+        public string Name { get; set; }
+
+        public something(string name)
+        {
+            Name = name;
+        }
+    }
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+
+            Console.WriteLine("What is happening here world?");
+            Console.ReadLine();
+        }
+    }
+}
