8380bfe9ef826d1e45ace9b822bf02fd10842c98
Nicolaj Iversen <NII@kmd.dk>
2016-12-20 15:29:21 +0100

    Done AccessModifiers

--- Create and use types/AccessModifiers.cs
+++ Create and use types/AccessModifiers.cs
@@ -5,6 +5,8 @@ using System.Runtime.InteropServices;
 using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
+using OtherNamespace;
+using dsfdfs.Create_and_use_types;
 
 namespace dsfdfs.Create_and_use_types
 {
@@ -12,39 +14,128 @@ namespace dsfdfs.Create_and_use_types
 	//Protected internal means.
 	//If in own assembly, access == internal(),
 	//if in other assembly, access == protected 
-	internal class AccessModifiers //Top level classes can only be 'internal' or 'public'
+	public class AccessModifier //Top level classes can only be 'internal' or 'public'
 	{
-		public string Name { get; set; }
+		public string AccessModifierName { get; set; }
 
-		protected class test //This is 'private as default, because its a nested class and therefore a member of class AccessModifiers
+		protected class Protec //This is 'private as default, because its a nested class and therefore a member of class AccessModifiers
 		{
-			public string Name2 { get; set; }
+			public string ProtecName { get; set; }
 		}
 
-		protected class test3 : test
+		//*************
+
+		internal protected class InternalProtec
 		{
-			public test something = new test();
-
-
+			public string internalProtecName { get; set; }
 		}
 
-		test3 bum = new test3();
+		//*************
 
 
+		internal class ClassInternal
+		{
+			public string InternalName { get; set; }
+		}
+
+		//*************
+
+	//**************** Example access in same container class in same namespace ************//
+	public void TryAccess()
+		{
+			Protec demo = new Protec();
+			demo.ProtecName = "I can access a protected class from whithin the containing class"; //No inheritence
+
+			InternalProtec demo1 = new InternalProtec();
+			demo1.internalProtecName = "I can access a protected class from whithin the containing class";
+
+			ClassInternal demo3 = new ClassInternal();
+			demo3.InternalName = "I can access a protected class from whithin the containing class";
+
+		}
+	}
+
+	//**************** Example access in different container class in same namespace ************//
+	public class AccessModifier2
+	{
+		//Cannot inherit from a protected class from another class in same namespace.
+		protected class InheritFromProtec : Protec
+		{
+		}
+
+		public void TryAccess2()
+		{
+			//I cannot access this class because its proteceted.
+			AccessModifier.Protec demo = new AccessModifier.Protec();
+			demo.ProtecName = "I can access a protected class from whithin the containing class"; //No inheritence
+
+			AccessModifier.InternalProtec demo1 = new AccessModifier.InternalProtec();
+			demo1.internalProtecName = "I can access a internal protected class from whithin another class in same assembly";
+
+			AccessModifier.ClassInternal demo3 = new AccessModifier.ClassInternal();
+			demo3.InternalName = "I can access a internal class from whithin another class in same assembly";
+		}
+	}
+
+	//**************** Example Inherit in own namespace ************//
+	public class AccessModifierInherited : AccessModifier
+	{
+		// Now I can access this class because the containing class has been inherited
+		protected class InheritFromProtec : Protec
+		{
+		}
+
+		public void TryAccess2()
+		{
+			// Now I can access this class because the containing class has been inherited
+			AccessModifier.Protec demo = new AccessModifier.Protec();
+			demo.ProtecName = "I can access a protected class from whithin the containing class"; //No inheritence
+
+			AccessModifier.InternalProtec demo1 = new AccessModifier.InternalProtec();
+			demo1.internalProtecName = "I can access a internal protected class from whithin another class in same assembly";
+
+			AccessModifier.ClassInternal demo3 = new AccessModifier.ClassInternal();
+			demo3.InternalName = "I can access a internal class from whithin another class in same assembly";
+		}
 	}
 }
 
+
+//**************** Example other namespace ************//
 namespace OtherNamespace
 {
-	internal class inheriteFromProtInternalClass : dsfdfs.Create_and_use_types.AccessModifiers
+	public class OtherAccessModifier 
 	{
-
-		protected class MyClass : dsfdfs.Create_and_use_types.AccessModifiers.test3
+		public void TryAccess2()
 		{
+			//I cannot access this class because its proteceted.
+			AccessModifier.Protec demo = new AccessModifier.Protec();
+			demo.ProtecName = "I can access a protected class from whithin the containing class"; //No inheritence
 
+			AccessModifier.InternalProtec demo1 = new AccessModifier.InternalProtec();
+			demo1.internalProtecName = "I can access a protected class from whithin another class in same assembly";
+
+			AccessModifier.ClassInternal demo3 = new AccessModifier.ClassInternal();
+			demo3.InternalName = "I can access a protected class from whithin another class in same assembly";
 		}
-
 	}
+}
+//**************** Example Inherit in other namespace ************//
+namespace OtherNamespace
+{
+	public class OtherAccessModifier2 : AccessModifier
+	{
+		public void TryAccess2()
+		{
+			//I cannot access this class because its proteceted.
+			AccessModifier.Protec demo = new AccessModifier.Protec();
+			demo.ProtecName = "I can access a protected class from whithin the containing class"; //No inheritence
 
+			AccessModifier.InternalProtec demo1 = new AccessModifier.InternalProtec();
+			demo1.internalProtecName = "I can access a protected class from whithin another class in same assembly";
 
+			AccessModifier.ClassInternal demo3 = new AccessModifier.ClassInternal();
+			demo3.InternalName = "I can access a protected class from whithin another class in same assembly";
+		}
+	}
 }
--- Program.cs
+++ Program.cs
@@ -6,18 +6,12 @@ using OtherNamespace;
 namespace dsfdfs
 {
 
-	public class Program
+	public class Program 
 	{
 		public static void Main(string[] args)
 		{
-			var test = new AccessModifiers();
-			test.Name = "jlkj";
-
-			var test2 = new inheriteFromProtInternalClass();
-			test2.Name = "fdsds";
-
-			var test3 = new Mu
-
+			
+		
 			//Reflection.Demo();
 			//StructStruct.Demo();
 			//Enum.Demo();
